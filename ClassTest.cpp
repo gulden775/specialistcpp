4d782f7d25812efcd9234eefed3475c75658c5c0
gulden <gulden775@gmail.com>
2023-10-12 13:25:21 +0300

    test commit github class Test

---
+++ ClassTest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class Test {
+
+	public:
+		int x;
+};
+
+int main(int argc, char** argv){
+
+	Test t1;
+	t1.x = 101;
+	std::cout << t1.x << std::endl;
+	
+	Test *t2 = new Test();
+	t2->x =202;
+	std::cout << t2->x << std::endl;
+	delete t2;
+
+	return 0;
+}
