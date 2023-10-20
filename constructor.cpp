6b925100633c762545f040e824001f84a66d91bb
gulden <gulden775@gmail.com>
2023-10-20 14:26:03 +0300

    destructor

--- constructor.cpp
+++ constructor.cpp
@@ -1,30 +1,39 @@
 #include <iostream>
 
 class Test{
-	public:
-	Test(void){
+	public:      //default constructor
+	Test(void):Test(0){
 	}
-   Test(int x){
-	   this->x = x;
+   Test(int x):x(x){		//full constructor
+
+
+	  // this->x = x;
    }
 
 int getX(void){
 	return x;
 }
+
+~Test(void){
+
+std::cout << "destructor" << std::endl;
+}
+
 	private:
 	int x;	
-
-
 };
 
 int main(int argc, char** argv){
 
-	Test t1; //Test *t1 = new Test();
+	Test t1; //Test *t1 = new Test();    //by pointer
 	std::cout << t1.getX() << std::endl;
 
 	Test t2(10);
 	std::cout << t2.getX() << std::endl;
 
+	Test t3=t2;
+	std::cout << t3.getX() << std::endl;
 
+	std::cout << "end of main"<< std::endl;
 return 0;
 }
